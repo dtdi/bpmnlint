8dfcff1d0b243d2525e14a8655d3cf3103f22b1f
Tobias Fehrer <tobias.fehrer@gmail.com>
2022-06-13 09:44:24 +0200

    linter

--- rules/no-duplicate-sequence-flows.js
+++ rules/no-duplicate-sequence-flows.js
@@ -1,41 +1,38 @@
-const {
-  is
-} = require('bpmnlint-utils');
+const { is } = require("bpmnlint-utils");
 
 /**
  * A rule that verifies that there are no disconnected
  * flow elements, i.e. elements without incoming
  * _or_ outgoing sequence flows
  */
-module.exports = function() {
-
+module.exports = function () {
   const keyed = {};
 
   const outgoingReported = {};
   const incomingReported = {};
 
   function check(node, reporter) {
-
-    if (!is(node, 'bpmn:SequenceFlow')) {
+    return;
+    if (!is(node, "bpmn:SequenceFlow")) {
       return;
     }
 
     const key = flowKey(node);
 
     if (key in keyed) {
-      reporter.report(node.id, 'SequenceFlow is a duplicate');
+      reporter.report(node.id, "SequenceFlow is a duplicate");
 
       const sourceId = node.sourceRef.id;
       const targetId = node.targetRef.id;
 
       if (!outgoingReported[sourceId]) {
-        reporter.report(sourceId, 'Duplicate outgoing sequence flows');
+        reporter.report(sourceId, "Duplicate outgoing sequence flows");
 
         outgoingReported[sourceId] = true;
       }
 
       if (!incomingReported[targetId]) {
-        reporter.report(targetId, 'Duplicate incoming sequence flows');
+        reporter.report(targetId, "Duplicate incoming sequence flows");
 
         incomingReported[targetId] = true;
       }
@@ -45,20 +42,18 @@ module.exports = function() {
   }
 
   return {
-    check
+    check,
   };
-
 };
-
 
 // helpers /////////////////
 
 function flowKey(flow) {
   const conditionExpression = flow.conditionExpression;
 
-  const condition = conditionExpression ? conditionExpression.body : '';
+  const condition = conditionExpression ? conditionExpression.body : "";
   const source = flow.sourceRef ? flow.sourceRef.id : flow.id;
   const target = flow.targetRef ? flow.targetRef.id : flow.id;
 
-  return source + '#' + target + '#' + condition;
+  return source + "#" + target + "#" + condition;
 }
